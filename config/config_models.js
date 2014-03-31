004242712a8c72327c7f969e182f3c5075c8c75c
Jayram Kumar <jayram.chandan@gmail.com>
2014-03-31 13:03:29 +0530

    adding user account model

--- config/config_models.js
+++ config/config_models.js
@@ -19,7 +19,9 @@ module.exports = function () {
         logs = require('../models/smslog'),
         smsMessageSentLogs = require('../models/smsMessageSentLogs'),
         redirect = require('../models/redirect'),
+        user_accounts = require('../models/userAccount.js'),
         beta_merchants = require('../models/beta_merchants');
+
     mongoose.model('Account').schema.add({
         account: {type: Schema.ObjectId, ref: 'Account'}
     });
