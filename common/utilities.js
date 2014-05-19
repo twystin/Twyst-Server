53a26880dd4f7c8af0d6bea365ce8c3ee96120d9
Jayram Kumar <jayram.chandan@gmail.com>
2014-05-19 14:46:38 +0530

    Changed rewardify function

--- common/utilities.js
+++ common/utilities.js
@@ -49,17 +49,29 @@ module.exports.rewardify = function (input) {
     if (input.reward.custom && input.reward.custom.text) {
         return input.reward.custom.text;
     } else if (input.reward.flat && (input.reward.flat.off || input.reward.flat.spend)) {
-        return "Get Rs." + ifEmpty(input.reward.flat.off) + " off on a minimum spend of Rs." + ifEmpty(input.reward.flat.spend);
+        if(input.reward.flat.off && input.reward.flat.spend) {
+            return "Get Rs." + ifEmpty(input.reward.flat.off) + " off on a minimum spend of Rs." + ifEmpty(input.reward.flat.spend);
+        }
+        if(input.reward.flat.off) {
+            return "Get Rs." + ifEmpty(input.reward.flat.off) + " off";
+        }
     } else if (input.reward.free && (input.reward.free.title || input.reward.free._with)) {
-        return "Get a free " + ifEmpty(input.reward.free.title) + " with " + ifEmpty(input.reward.free._with);
+        if(input.reward.free.title && input.reward.free._with) {
+            return "Get a free " + ifEmpty(input.reward.free.title) + " with " + ifEmpty(input.reward.free._with);
+        }
+        if(input.reward.flat.off) {
+            return "Get a free " + ifEmpty(input.reward.free.title);
+        }
     } else if (input.reward.buy_one_get_one && input.reward.buy_one_get_one.title) {
         return "Get " + ifEmpty(input.reward.buy_one_get_one.title);
     } else if (input.reward.reduced && (input.reward.reduced.what || input.reward.reduced.worth || input.reward.reduced.for_what)) {
-        return "Get " + ifEmpty(input.reward.reduced.what) + " worth Rs. " + ifEmpty(input.reward.reduced.worth) + " for Rs. " + ifEmpty(input.reward.reduced.for_what);
+        if(input.reward.reduced.what && input.reward.reduced.worth) {
+           return "Get " + ifEmpty(input.reward.reduced.what) + " worth Rs. " + ifEmpty(input.reward.reduced.worth) + " for Rs. " + ifEmpty(input.reward.reduced.for_what);
+        }
     } else if (input.reward.happyhours && input.reward.happyhours.extension) {
         return "Extended happy hours by " + ifEmpty(input.reward.happyhours.extension);
     } else if (input.reward.discount) {
-        if (!input.reward.discount.max) {
+        if (input.reward.discount.max) {
             return "Get " + ifEmpty(input.reward.discount.percentage) + " off, subject to a maximum discount of Rs." + ifEmpty(input.reward.discount.max);
         } else {
             return "Get " + ifEmpty(input.reward.discount.percentage) + " off";
