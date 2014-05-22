2dbeb6c634f745f24e1147a34d56150425672491
Jayram Kumar <jayram.chandan@gmail.com>
2014-05-22 14:03:50 +0530

    Adding batcher

---
+++ gcmBatcher.js
@@ -0,0 +1,43 @@
+var gcm = require('node-gcm');
+var csv = require('csv');
+var fs = require('fs');
+
+//API Server Key
+var sender = new gcm.Sender('AIzaSyBi6MTa4P26485PLLo6-LFCMrT-H5o-b5w');
+
+csv()
+.from.stream(fs.createReadStream(__dirname + '/notifications.csv', { encoding: 'utf8' }))
+.on('record', function (row, index) {
+    if (index == 0) {
+        var gcm_id = row[0];
+        var message = new gcm.Message();
+
+		// Value the payload data to send...
+		message.addData('message', row[1]);
+		message.addData('title','Welcome to Twyst' );
+		message.addData('msgcnt','1'); // Shows up in the notification in the status bar
+		message.addData('soundname','beep.wav'); //Sound to play upon notification receipt - put in the www folder in app
+		//message.collapseKey = 'demo';
+		//message.delayWhileIdle = true; //Default is false
+		message.timeToLive = 3000;// Duration in seconds to hold in GCM and retry before timing out. Default 4 weeks (2,419,200 seconds) if not specified.
+
+        sendPush(message, gcm_id);
+		/**
+		 * Parameters: message-literal, registrationIds-array, No. of retries, callback-function
+		 */
+    }
+})
+.on('end', function (count) {
+    console.log("Message sent to all the users");
+})
+.on('error', function (error) {
+    console.log(error.message);
+});
+
+function sendPush(message, id) {
+	var registrationIds = [];
+	registrationIds.push(id);
+	sender.send(message, registrationIds, 4, function (result) {
+	    console.log(result);
+	});
+}
