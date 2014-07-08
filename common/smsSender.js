082e443d772eb10ded2d2a2b7be6513cc608c7b8
Jayram <jayram.chandan@gmail.com>
2014-07-08 09:55:02 +0530

    Tracking message real status

--- common/smsSender.js
+++ common/smsSender.js
@@ -12,31 +12,34 @@ module.exports.sendSms = function (phone, push_message) {
 	var message = push_message.replace(/&/g,'%26');
 	message = message.replace(/% /g,'%25 ');
 	console.log(message);
-	saveSentSms(phone, message);
 	var send_sms_url = sms_push_url + phone + "&from=TWYSTR&udh=0&text=" + message;
 	var test_url = 'http://staging.twyst.in/api/v2/sms/status';
-	http.post(send_sms_url, function(res){
+	http.get(send_sms_url, function(res){
 		console.log(res.statusCode);
+		var body = '';
 		res.on('data', function(chunk) {
             // append chunk to your data
             body += chunk;
         });
 
         res.on('end', function() {
+        	saveSentSms(phone, message, body);
             console.log(body);
         });
 
         res.on('error', function(e) {
+        	saveSentSms(phone, message, e);
             console.log("Error message: " + e.message)
         });
 	});
 }
 
-function saveSentSms (phone, message) {
+function saveSentSms (phone, message, status) {
 
 	var sms_log = {};
 	sms_log.phone = phone;
 	sms_log.message = message;
+	sms_log.status = status;
 	
 	var sms_log = new SmsSentLog(sms_log);
 
