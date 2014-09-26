1a99b7f5cb6f78e58d97e4e272d606c6a9a833c4
Jayram <jayram.chandan@gmail.com>
2014-09-26 15:34:39 +0530

    Confused in second and millisecond

--- controllers/checkin.js
+++ controllers/checkin.js
@@ -329,7 +329,7 @@ function detectThirtyMinutesCap(qr, req, res, outlet_id, phone, checkin_type, ch
 		}
 		else {
 			var diff = Date.now() - checkin.created_date;
-			if(diff > 5000) {
+			if(diff > 5 * 60 * 1000) {
 				create(req, res, outlet_id, phone, checkin_type, checkin_code, checkin_location);				
 			}
 			else {
