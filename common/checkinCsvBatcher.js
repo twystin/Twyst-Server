d0642cda95af7244b47250b93bb4d0ed9c1650d3
Jayram <jayram.chandan@gmail.com>
2014-10-11 14:49:26 +0530

    Adding awesome restler lib...its too goood

--- common/checkinCsvBatcher.js
+++ common/checkinCsvBatcher.js
@@ -1,5 +1,4 @@
-var http = require('http');
-http.post = require('http-post');
+var rest = require('restler');
 var csv = require('csv');
 var fs = require("fs");
 
@@ -19,28 +18,16 @@ csv()
 
 function httpCheckin (phone) {
 	console.log(phone)
-	http.post('http://localhost:3000/api/v2/batch_checkins', {
-		phone: phone,
-        outlet: "5402cc64ad0d0ffd5aa44817",
-        location: 'DINE_IN',
-        created_date: new Date(),
-        batch_user: true,
-        message: "We love serving you at Captain Bill$ Deliverz! As a small way of saying thanks, we have enrolled you into our new rewards program on Twyst. We also have a special offer for you this October - 1 plus 1 on any Soup, Starter or Biryani for additional Rs. 50* only! Check-in on Twyst on each order for bigger rewards. Get Twyst http://twy.st/app"
-	},
-	function(res){
-		console.log(res.statusCode);
-		var body;
-		res.on('data', function(chunk) {
-	        // append chunk to your data
-	        body += chunk;
-	    });
-
-	    res.on('end', function() {
-	        console.log(body);
-	    });
-
-	    res.on('error', function(e) {
-	        console.log("Error message: " + e.message)
-	    });
-	});
+	rest.post('http://localhost:3000/api/v2/batch_checkins', {
+		data: {
+			phone: phone,
+	        outlet: "5402cc64ad0d0ffd5aa44817",
+	        location: 'DINE_IN',
+	        created_date: new Date(),
+	        batch_user: true,
+	        message: "We love serving you at Captain Bill$ Deliverz! As a small way of saying thanks, we have enrolled you into our new rewards program on Twyst. We also have a special offer for you this October - 1 plus 1 on any Soup, Starter or Biryani for additional Rs. 50* only! Check-in on Twyst on each order for bigger rewards. Get Twyst http://twy.st/app"
+		}
+	}).on('complete', function(data, response) {
+	  	console.log(data)
+	})
 }
