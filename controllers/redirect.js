61fa578b9e4866d183f26930826f4f0292c9391a
Jayram <jayram.chandan@gmail.com>
2014-10-25 10:07:15 +0530

    Removing all that crap

--- controllers/redirect.js
+++ controllers/redirect.js
@@ -37,38 +37,17 @@ module.exports.redirectToOutlet = function (req, res) {
 			redirect(null);
 		}
 		else {
-			redirect(outlet);
+			console.log(outlet)
+			redirect(outlet.publicUrl[0])
 		}
 	})
 
-	function redirect(outlet) {
-		if(!outlet) {
+	function redirect(url) {
+		if(!url) {
 			res.redirect('/');
 		}
 		else {
-			res.send(renderPage(outlet));
+			res.redirect('/outlets/#/' + url);
 		}
-	}
-
-	function renderPage (outlet) {
-		var html = '';
-		html += '\n<!doctype html>';
-		html += '\n<html ng-app="outletApp">';
-		html += '\n\t<head>';
-		html += '\n\t<title>'+ outlet.basics.name + ', ' + 
-			outlet.contact.location.locality_1.toString() + ', ' + 
-			outlet.contact.location.locality_2.toString() + ', ' +
-			outlet.contact.location.city + ' - Twyst</title>';
-		html += '<meta property="og:site_name" content="Twyst Outlet"/>';
-		html += '<meta property="og:title" content="'+outlet.basics.name+', ' + outlet.contact.location.locality_1[0]+', Twyst" />';
-		html +=	'<meta property="og:url" content="http://staging.twyst.in/outlets/#/'+outlet.publicUrl[0]+'" />';
-		html +=	'<meta property="og:description" content="Unlock exclusive rewards for being a regular! Check in on Twyst every time you visit us or order." />';
-		html += '<meta property="og:image" content="https://s3-us-west-2.amazonaws.com/twystmerchantpages/merchants/'+outlet.basics.slug+'/logo.png" />';
-		html += '<script>window.location.href = "http://staging.twyst.in/'+ outlet.publicUrl[0] +'"</script>';
-		html += '\n\t</head>';
-		html += '\n\t<body>';
-		html += '\n\t</body>'
-		html += '</html>'
-		res.send(html);
 	}
 }
