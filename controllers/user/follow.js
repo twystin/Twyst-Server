bd886b6f0e29f97567325db0263e132f38307372
Jayram <jayram.chandan@gmail.com>
2014-11-20 12:55:36 +0530

    Unfollow outlet

--- controllers/user/follow.js
+++ controllers/user/follow.js
@@ -1,7 +1,87 @@
-module.exports.follow = function () {
+var mongoose = require('mongoose');
+var Follow = mongoose.model('Favourite');
 
+module.exports.follow = function (req, res) {
+	if(!req.body.outlet) {
+		res.send(400, {
+			'status': 'error',
+			'message' : 'Error in request',
+			'info': null
+		})
+	}
+	else {
+		var follow = {
+			account: req.user._id,
+			outlets: req.body.outlet
+		};
+		follow.findOne(follow, function (err, f) {
+			if(err) {
+				res.send(400, {
+					'status': 'error',
+					'message' : 'Error saving follow',
+					'info': err
+				})
+			}
+			else {
+				if(follow) {
+					res.send(200, {
+						'status': 'success',
+						'message': 'Outlet already followed',
+						'info': follow
+					})
+				}
+				else {
+					follow = new Follow(follow);
+					follow.save(function (err, follow) {
+						if(err) {
+							res.send(400, {
+								'status': 'error',
+								'message' : 'Error saving follow',
+								'info': err
+							})
+						}
+						else {
+							res.send(200, {
+								'status': 'success',
+								'message': 'Saved follow outlet',
+								'info': follow
+							})
+						}
+					})
+				}
+			}
+		})
+	}
 }
 
 module.exports.unfollow = function () {
-	
+	if(!req.body.outlet) {
+		res.send(400, {
+			'status': 'error',
+			'message' : 'Error in request',
+			'info': null
+		})
+	}
+	else {
+		var follow = {
+			account: req.user._id,
+			outlets: req.body.outlet
+		};
+		Follow.remove(follow, function (err) {
+			if(err) {
+				res.send(400, {
+					'status': 'error',
+					'message' : 'Error unfollowing outlet',
+					'info': err
+				})
+			}
+			else {
+				res.send(200, {
+					'status': 'success',
+					'message': 'Unfollowed outlet',
+					'info': null
+				})
+			}
+		})
+	}
 }
