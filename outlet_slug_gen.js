0deec222831299132b417a0791a4c3bbd6305bdb
Jayram <jayram.chandan@gmail.com>
2014-11-25 11:14:21 +0530

    Job for outlet public url

--- outlet_slug_gen.js
+++ outlet_slug_gen.js
@@ -8,36 +8,36 @@ mongoose.connect('mongodb://50.112.253.131/twyst');
 
 Outlet.find({}, function (err, outlets) {
 	outlets.forEach(function (o) {
-		var city = o.contact.location.city.toLowerCase();
-		// if(city === 'delhi' 
-		// 	|| city === 'gurgaon' 
-		// 	|| city === 'noida' 
-		// 	|| city === 'new delhi') {
-		// 	var url = '/' + 'ncr' + '/' +
-		// 		o.contact.location.locality_2[0] + '/' +
-		// 		o.basics.name + ' ' + o.contact.location.locality_1[0] + ' ' +
-		// 		o.contact.location.city;
-		// }
-		// else {
-		// 	var url = '/' + o.contact.location.city + '/' + 
-		// 		o.contact.location.locality_2[0] + '/'
-		// 		o.basics.name + ' ' + o.contact.location.locality_1[0];
-		// }
-		var url = o.basics.name + ' ' + o.contact.location.locality_1[0] + ' ' +
-				o.contact.location.city;
-		url = url.replace(/'/g, '');
-		url = url.replace(/& /g, '');
-		url = url.replace(/- /g, '');
-		url = url.replace(/\./g, '');
-		url = url.replace(/!/g, '');
-		url = url.replace(/ /g, '-');
-		o.publicUrl = o.publicUrl || [];
-		o.publicUrl.push(url.toLowerCase());
-		console.log(url)
+		o.publicUrl = getPublicUrl(o);
+		console.log(o.publicUrl)
 		o.save(function (err) {
 			if(err) {
 				console.log(err)
 			}
 		})
 	})
-})
+});
+
+function getPublicUrl(o) {
+	var publicUrl = [];
+	if(!o.contact
+		|| !o.contact.location
+		|| !o.contact.location.city
+		|| !(o.contact.location.locality_1 
+				&& o.contact.location.locality_1.length)
+		|| !(o.contact.location.locality_1 
+				&& o.contact.location.locality_1.length)) {
+		return publicUrl;
+	}
+	var url = o.basics.name 
+		+ ' ' 
+		+ o.contact.location.locality_1
+		+ ' ' 
+		+ o.contact.location.city;
+
+	url = url.replace(/[^a-zA-Z0-9 ]/g, "");
+	url = url.replace(/\s{1,}/g, '-');
+	url = url.toLowerCase();
+	publicUrl.push(url);
+	return publicUrl;
+}
