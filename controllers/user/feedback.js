3d3a4c89c07912f8c9eb662043d1f3dd3a1cfca1
Jayram <jayram.chandan@gmail.com>
2014-11-27 10:59:23 +0530

    Image to base 64

--- controllers/user/feedback.js
+++ controllers/user/feedback.js
@@ -56,12 +56,9 @@ module.exports.save = function(req, res) {
 	}
 
 	function decodeBase64Image(dataString) {
+		dataString = "data:image/jpeg;base64" + dataString;
 		var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
 		response = {};
-
-		if (matches.length !== 3) {
-			return new Error('Invalid input string');
-		}
 
 		response.type = matches[1];
 		response.data = new Buffer(matches[2], 'base64');
