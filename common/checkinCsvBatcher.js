4ca025407a1708df4710286fe532d1c4bed682ae
Jayram <jayram.chandan@gmail.com>
2014-12-22 18:59:10 +0530

    Multiply -1

--- common/checkinCsvBatcher.js
+++ common/checkinCsvBatcher.js
@@ -1,33 +1,42 @@
 var rest = require('restler');
 var csv = require('csv');
 var fs = require("fs");
+var async = require('async');
 readCsv(0)
-var phones = '';
+var phones = [];
 function readCsv(data_index) {
 	csv()
-	.from.stream(fs.createReadStream(__dirname + '/grub.csv', { encoding: 'utf8' }))
-	.on('record', function (row, index) {		
-	    if (index === data_index) {
-	    	httpCheckin(index, row[0]);
-	    }
+	.from.stream(fs.createReadStream(__dirname + '/smug.csv', { encoding: 'utf8' }))
+	.on('record', function (row, index) {	
+		if(index > 1000) {
+			phones.push(row[0]);
+		}
+	    // if (index === data_index) {
+	    // 	httpCheckin(index, row[0]);
+	    // }
 	})
 	.on('end', function (count) {
 		//console.log(count)
 		console.log("I am finished.")
+		phones.forEach(function (phone) {
+			httpCheckin(phone, function (err) {
+				
+			})
+		});
 	})
 }
 
-function httpCheckin (index, phone) {
+function httpCheckin (phone, cb) {
 	console.log(phone)
-	rest.post('http://twyst.in/api/v2/batch_checkins', {
+	rest.post('http://twyst.in/api/v3/batch_checkins', {
 		data: {
 			phone: phone,
-	        outlet: "54869eea9f95ddc7429b022c",
+	        outlet: "5491a7abd031388c54653b35",
 	        location: 'DINE_IN',
-	        message: "We love serving you at Captain Grub! As a way of saying thanks, we've enrolled you into our new rewards program on Twyst. Get a Free Dessert on your next order (Voucher code: xxxxxx. Minimum bill Rs 999)! See all rewards at http://twyst.in/grub"
+	        message: "We've loved serving you at Smugglers! As a way of saying thanks, we've enrolled you into our rewards program on Twyst. Get a free drink with your meal on your next visit (Voucher code: xxxxxx. Min. bill Rs 500). See all our rewards at http://twyst.in/smu Special offer: Get 15% off on all orders till 31-Dec!"
 		}
 	}).on('complete', function(data, response) {
-	  	console.log(data)
-	  	readCsv(++index);
+	  	//console.log(data)
+	  	cb(null);
 	})
 }
--- controllers/user/recco.js
+++ controllers/user/recco.js
@@ -317,6 +317,7 @@ function computeReccoWeight(object_set, checkin_data, lat, lon) {
 			o.outlet_summary) * 100 / checkin_data.total_checkins;
 		o.distance = calculateDistance(o.outlet_summary, lat, lon)
 		o.total = o.popularity + (100 - o.distance);
+		o.total *= -1;
 	});
 	return object_set;
 }
