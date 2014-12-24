ac1ab510620ef6c71fbf97726d0ddbe5ab74430c
Jayram <jayram.chandan@gmail.com>
2014-12-24 20:19:19 +0530

    Checkin csv batch

--- common/checkinCsvBatcher.js
+++ common/checkinCsvBatcher.js
@@ -6,14 +6,9 @@ readCsv(0)
 var phones = [];
 function readCsv(data_index) {
 	csv()
-	.from.stream(fs.createReadStream(__dirname + '/smug.csv', { encoding: 'utf8' }))
+	.from.stream(fs.createReadStream(__dirname + '/crus.csv', { encoding: 'utf8' }))
 	.on('record', function (row, index) {	
-		if(index > 1000) {
-			phones.push(row[0]);
-		}
-	    // if (index === data_index) {
-	    // 	httpCheckin(index, row[0]);
-	    // }
+		phones.push(row[0]);
 	})
 	.on('end', function (count) {
 		//console.log(count)
@@ -27,16 +22,16 @@ function readCsv(data_index) {
 }
 
 function httpCheckin (phone, cb) {
-	console.log(phone)
 	rest.post('http://twyst.in/api/v3/batch_checkins', {
 		data: {
 			phone: phone,
-	        outlet: "5491a7abd031388c54653b35",
+	        outlet: "5316d59326b019ee59000026",
 	        location: 'DINE_IN',
-	        message: "We've loved serving you at Smugglers! As a way of saying thanks, we've enrolled you into our rewards program on Twyst. Get a free drink with your meal on your next visit (Voucher code: xxxxxx. Min. bill Rs 500). See all our rewards at http://twyst.in/smu Special offer: Get 15% off on all orders till 31-Dec!"
+	        message: 'Merry Christmas from Crusty! Here is a holiday gift specially for you: 1 free check-in into our new rewards program on Twyst! You get a free 6" Garlic & Herbs pizza on your next visit to our new Baani Square outlet (Voucher code: xxxxxx). Happy holidays!'
 		}
 	}).on('complete', function(data, response) {
 	  	//console.log(data)
+	  	console.log(phone)
 	  	cb(null);
 	})
 }
