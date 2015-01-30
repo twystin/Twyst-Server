6768af2859e49be566c7f3095350829abfc7e526
Jayram <jayram.chandan@gmail.com>
2015-01-30 14:05:22 +0530

    Hit the changed message

--- controllers/checkins/panel/request.js
+++ controllers/checkins/panel/request.js
@@ -144,7 +144,7 @@ function checkinMessageSender(_obj, outlet) {
 	var _message = null;
 	if(_obj.count === 0) {
 		if(_obj.voucher) {
-			_message = 'Welcome to the '+ outlet.basics.name +' rewards program on Twyst!. Check-in successful on '+ Utils.formatDate(new Date(_obj.created_date)) +'. Reward unlocked - yay! You will soon receive your voucher code via SMS. Find '+ outlet.basics.name +' on Twyst: http://twyst.in/'+ outlet.shortUrl[0];
+			_message = 'Welcome to the '+ outlet.basics.name +' rewards program on Twyst!. Check-in successful on '+ Utils.formatDate(new Date(_obj.created_date)) +'. Reward unlocked! Your voucher will be available on your Twyst app soon. Don’t have the app? Get it now at http://twy.st/app';
 		}
 		else {
 			_message = 'Welcome to the '+ outlet.basics.name +' rewards program on Twyst!. Check-in successful on '+ Utils.formatDate(new Date(_obj.created_date)) +'. You are '+ _obj.reward_distance +' check-in(s) away from your next reward. Find '+ outlet.basics.name +' on Twyst: http://twyst.in/'+ outlet.shortUrl[0];
