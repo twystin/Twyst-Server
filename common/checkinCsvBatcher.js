1b72b383684294843a74999e78a3a211819db99d
Jayram <jayram.chandan@gmail.com>
2015-02-10 12:45:37 +0530

    Improved batch checkin code

--- common/checkinCsvBatcher.js
+++ common/checkinCsvBatcher.js
@@ -2,40 +2,55 @@ var rest = require('restler');
 var csv = require('csv');
 var fs = require("fs");
 var async = require('async');
-readCsv(0)
-var phones = [];
-function readCsv(data_index) {
-	csv()
-	.from.stream(fs.createReadStream(__dirname + '/ccc.csv', { encoding: 'utf8' }))
-	.on('record', function (row, index) {	
-		if(index > data_index) {
-			phones.push(row[0]);
-		}
-	})
-	.on('end', function (count) {
-		console.log(count)
-		async.eachSeries(phones, function (phone, callback) {
-			httpCheckin(phone, function (err) {
+
+var config = {
+	'csv_file_name': '/ccc.csv', // File path with phone numbers
+	'checkin_url': 'http://twyst.in/api/v3/batch_checkins',
+	'sms_sender_id': 'COFCHA',
+	'outlet_id': '5332a73e4871e79576000c3a',
+	'checkin_location': 'DINE_IN',
+	'message': "Greetings from Coffee & Chai Co! We've enrolled you into our new rewards program on Twyst. Get a cup of Tea/Coffee free when you visit us next (Voucher Code xxxxxx, T&C: Min spend Rs 200). Get the Twyst app (http://twy.st/app) to check-in, unlock and Redeem your Rewards!"
+};
+
+initCheckin();
+function initCheckin() {
+	getPhoneNumbersFromFile(config.csv_file_name, function (phone_numbers) {
+		async.each(phone_numbers, function (phone, callback) {
+			httpCheckin(phone, function (data, response) {
+				console.log(data);
+				//console.log(response);
 				callback();
 			});
 		}, function (err) {
-			console.log(err);
+			console.log('---------------------------------------');
+			console.log(err || 'Completed Batch checkin process');
 		});
-	})
-}
+	});
+};
 
 function httpCheckin (phone, cb) {
-	rest.post('http://twyst.in/api/v3/batch_checkins', {
+	rest.post(config.checkin_url, {
 		data: {
-			sms_sender_id: 'COFCHA',
+			sms_sender_id: config.sms_sender_id,
 			phone: phone,
-	        outlet: "5332a73e4871e79576000c3a",
-	        location: 'DINE_IN',
-	        message: "Greetings from Coffee & Chai Co! We've enrolled you into our new rewards program on Twyst. Get a cup of Tea/Coffee free when you visit us next (Voucher Code xxxxxx, T&C: Min spend Rs 200). Get the Twyst app (http://twy.st/app) to check-in, unlock and Redeem your Rewards!"
+	        outlet: config.outlet_id,
+	        location: config.checkin_location,
+	        message: config.message
 		}
 	}).on('complete', function(data, response) {
-	  	console.log(data)
-	  	console.log(phone)
-	  	cb(null);
+		cb(data, response);	
+	});
+};
+
+function getPhoneNumbersFromFile(file_name, cb) {
+	var phone_numbers = [];
+	csv()
+	.from
+	.stream(fs.createReadStream(__dirname + file_name, { encoding: 'utf8' }))
+	.on('record', function (row, index) {	
+		phone_numbers.push(row[0]);
 	})
-}
+	.on('end', function (count) {
+		cb(phone_numbers);
+	})
+};
