3e05452ed0e0fcdce025e4550ed5a4910808b1d8
arunr <arunr@outlook.com>
2015-03-20 00:34:47 +0530

    Fixed up birthdayImporter code

--- jobs/birthday/birthdayImporter.js
+++ jobs/birthday/birthdayImporter.js
@@ -1,22 +1,22 @@
 var csv = require('csv');
 var fs = require("fs");
-var account = require('../models/account.js');
+var account = require('../../models/account.js');
 
 var mongoose = require('mongoose');
 var Account = mongoose.model('Account');
 
-mongoose.connect('mongodb://50.112.253.131/twyst');
+mongoose.connect('mongodb://localhost/twyst');
 
 csv()
-	.from.stream(fs.createReadStream(__dirname + '/birthdays.csv', { encoding: 'utf8' }))
+	.from.stream(fs.createReadStream(__dirname + '/birthdays2.csv', { encoding: 'utf8' }))
 	.on('record', function (row, index) {
-	    if (index === 1) {
-	    	Account.findOne({phone:row[4]}, function(e,a) {
+	    if (index > 0) {
+	    	Account.findOne({phone:row[8]}, function(e,a) {
 	    	if (e) {
-	    		//console.log(e);
+	    		console.log("ERROR " + e);
 	    	} else {
 	    		if (!a) {
-	    			//console.log("Not found!");
+	    			console.log("ERROR: Account not found: " + row[8]);
 	    		} else {
 	    			console.log("Saving")
 	    			a.profile.bday.y = row[7];
@@ -24,18 +24,18 @@ csv()
 	    			a.profile.bday.d = row[5];
 	    			a.profile.first_name = row[1] || '';
 	    			a.profile.last_name = row[3] || '';
-	    			a.profile.email = row[8] || '';
+	    			a.profile.email = row[4] || '';
 	    			a.save(function(s, err) {
-	    				console.log(s);
-	    				console.log(err);
+	    				// console.log(s);
+	    				// console.log(err);
 	    			});
 	    		}
-	    		//console.log(a);
+	    		// console.log(a);
 	    	}
 	    })
 	    }
 
 	})
 	.on('end', function (count) {
-		//console.log("I am finished.")
+		console.log("Finished reading the CSV file")
 	})
