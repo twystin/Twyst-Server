49a8b72b64c1aff3a4c498fa654032f73141f767
Kuldeep Patel <kuldeepp89@gmail.com>
2015-05-01 14:56:15 +0530

    deals code

---
+++ controllers/deal.js
@@ -0,0 +1,88 @@
+// Load up the models required.
+var mongoose = require('mongoose');
+var Voucher = mongoose.model('Voucher');
+var	Deal = mongoose.model('Deal');
+var Account = mongoose.model('Account');
+var keygen = require("keygenerator");
+
+
+module.exports.processDeal = function(req, res) {
+	var deal = req.params.deal;
+	var user = req.user;
+	console.log('deal id ' + req.params.deal)
+	Deal.find({'_id': deal},  function(err, gotDeal) {
+		if(err) console.log('error in getting deal ' + err);
+		console.log('gotDeal' +gotDeal);
+		
+		saveVoucher(gotDeal, user, function(err, callback) {
+			if(err) {
+				console.log(err)
+				res.send(400, {
+					'status': 'error',
+					'message': 'There is somthing wrong',
+					'info': err
+				});
+			}
+			else {
+				res.send(200, {
+					'status': 'success',
+					'message': 'Voucher successfully generated for deal',
+					'info': Voucher
+				});
+			}
+
+		})
+	})
+
+}
+
+// Get the voucher object and save it!
+function saveVoucher(deal, user, cb) {
+	var voucher = getVoucherObject(deal, user);
+	voucher = new Voucher(voucher);
+	
+	voucher.save(function (err) {
+		if(err) {
+			cb(err, voucher);
+		}
+		else {
+			cb(null, voucher);
+		}
+		
+	})
+	 
+
+}
+
+// Generate the voucher object for the deal.
+function getVoucherObject(deal, user) {
+	var voucher = {
+		basics: {
+			code: keygen._({
+				forceUppercase: true,
+				length: 6,
+				exclude:['O', '0', 'L', '1']
+			}),
+			description: deal.detail,
+			created_at: new Date(),
+			modified_at: new Date(),
+			status: 'active',
+			type: 'DEAL'
+		},
+		terms: deal.tc,
+		validity: {
+			start_date: new Date(),
+			end_date: Date.now() + 7 * 86400000
+		},
+		issue_details: {
+			issue_date: new Date(),
+			issued_at: deal.outlets.map(function (o) {
+				return o._id;
+			}),
+			issued_to: user._id
+		}
+	}
+	return voucher;
+}
+
+
