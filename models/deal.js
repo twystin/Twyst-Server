e16be317a731c51f4df68d0ae9f70ef4a8ddddf3
arunr <arunr@outlook.com>
2015-05-06 18:01:31 +0530

    Changes to deal structure

--- models/deal.js
+++ models/deal.js
@@ -5,13 +5,22 @@ var business_hours = require("../common/operatingHours")
 
 var DealSchema = new Schema ({
     detail: {type: String, trim: true, required: true},
+    info: {type: String},
     tc: {type: String, trim: true, required: true},
     slug: {type: String, trim: true, required: true},
     created_at : {type: Date, default: Date.now},
     modified_at: {type: Date, default: Date.now},
     end_date: {type: Date, default: Date.now},
     status: {type: String, enum: ['active', 'archived', 'draft'], default: 'draft'},
-    avaiable_at: business_hours.hours,
+    avaiable_at: {
+      '1': {s:{h:Number,m:Number}, e:{h:Number, m:Number}},
+      '2': {s:{h:Number,m:Number}, e:{h:Number, m:Number}},
+      '3': {s:{h:Number,m:Number}, e:{h:Number, m:Number}},
+      '4': {s:{h:Number,m:Number}, e:{h:Number, m:Number}},
+      '5': {s:{h:Number,m:Number}, e:{h:Number, m:Number}},
+      '6': {s:{h:Number,m:Number}, e:{h:Number, m:Number}},
+      '7': {s:{h:Number,m:Number}, e:{h:Number, m:Number}}
+    },
     generate_coupon: {type: Boolean},
     outlets: [{type: Schema.ObjectId, ref: 'Outlet'}]
 });
